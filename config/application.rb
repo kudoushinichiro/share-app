b1abe08177b1b69b2e36ea37252f96d061c90904
kudoushinichiro <shin.kudo.aa@gmail.com>
2020-08-09 14:28:48 +0900

    i18nの導入

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,13 @@ module ShareApp
     config.generators do |g|
       g.skip_routes   true
       g.assets false
-      g.test_framework false 
+      g.helper false
     end
+
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
   end
 end
