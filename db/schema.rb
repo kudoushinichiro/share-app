20e363b0b33f68583cd4f3a6f40d178a7560b2d3
kudoushinichiro <shin.kudo.aa@gmail.com>
2020-08-11 10:23:36 +0900

    rubocopの指摘修正（username-index）

--- db/schema.rb
+++ db/schema.rb
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2020_08_03_113412) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
