0a69cd6e8c1b5efbf756afdd0817a455197ea13c
kudoushinichiro <shin.kudo.aa@gmail.com>
2020-08-16 16:52:43 +0900

    modelファイルの設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,14 @@
 class Post < ApplicationRecord
+  mount_uploaders :images, ImageUploader
+  # uploaderファイルをimagesカラムに適用
 
   belongs_to :user
   # postは１つのuserを持つ、というアソシエーション
   # この記載がないと、参照先(この場合はuser)テーブルにアクセスできない
+
+  validates :body, presence: true, length: { maximum: 255 }
+  validates :images, presence: true
+  # migrationファイルでnullfalseをつけたものはこちらでもバリデーションをつける。
+  # bodyはなんとなく最大255文字までにしたけど、一般的に何文字くらいにするのがよいのだろう？
 
 end
