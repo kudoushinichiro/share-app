1e22766e87a62ecc8daccacf63acef5e6668d448
kudoushinichiro <shin.kudo.aa@gmail.com>
2020-08-21 23:30:37 +0900

    seedデータ修正（画像取得）

--- db/seeds/posts.rb
+++ db/seeds/posts.rb
@@ -4,7 +4,7 @@ User.limit(3).each do |user|
   # 以下がuser数分繰り返し実行される
   # limit(n)と制限をつけると、user_idの1〜n番までのuserがダミーのpostを実行する
   # 仮にこの中に正規のuserがいてもseedデータの投入がされる模様。注意
-  post = user.posts.create(body: Faker::Coffee.blend_name, images: [open("#{Rails.root}/db/fixtures/dummy.png")])
+  post = user.posts.create(body: Faker::Coffee.blend_name, remote_images_urls: %w[https://picsum.photos/350/350/?random https://picsum.photos/350/350/?random])
   puts "post#{post.id}が作成されました"
 
 end
