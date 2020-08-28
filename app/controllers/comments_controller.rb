ba76fcb794974d3695f1227363c3ae103d90d382
kudpig <shin.kudo.aa@gmail.com>
2020-08-28 17:43:31 +0900

    コメントの機能(edit/update/destroy)

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,27 @@
 class CommentsController < ApplicationController
+  before_action :require_login, only: %i[create edit update destroy]
+  # require_loginメソッドにより、ログインしていないユーザは上記のアクションを実行できない
 
   def create
     @comment = current_user.comments.create(comment_params)
     # current_userを指定してコメントのcreateを実行する
+  end
+
+  # editはshallowの対象
+  def edit
+    @comment = current_user.comments.find(params[:id])
+  end
+
+  # updateはshallowの対象
+  def update
+    @comment = current_user.comments.find(params[:id])
+    @comment.update(comment_update_params)
+  end
+
+  # destroyはshallowの対象
+  def destroy
+    @comment = current_user.comments.find(params[:id])
+    @comment.destroy
   end
 
   private
@@ -14,4 +33,8 @@ class CommentsController < ApplicationController
     # current_user.comments.create(comment_params)ですでにuser_idを取得できている？
   end
 
+  def comment_update_params
+    params.require(:comment).permit(:body)
+    # updateするものはbodyのみ。それ以外の項目が変更されることは基本的にないため許可する必要もない
+  end
 end
