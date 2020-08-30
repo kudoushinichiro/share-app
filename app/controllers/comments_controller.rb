e380d4e599ff864cf0c2be5933867217af634492
kudpig <shin.kudo.aa@gmail.com>
2020-08-31 07:36:39 +0900

    修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -27,7 +27,7 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    params.require(:comment).permit(:body).merge(user_id: current_user.id, post_id: params[:post_id])
+    params.require(:comment).permit(:body).merge(post_id: params[:post_id])
     # user_idとpost_idをcommentに紐づけて保存出来るようにする
     # もしかしてmerge(user_id: current_user.id)って必要ないのでしょうか？
     # current_user.comments.create(comment_params)ですでにuser_idを取得できている？
