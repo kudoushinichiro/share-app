bde991bd90a043c8e97dab78fd9568a94fc4e501
kudpig <shin.kudo.aa@gmail.com>
2020-09-06 16:04:16 +0900

    DBにユニーク制約を追加

--- db/migrate/20200906063609_create_relationships.rb
+++ db/migrate/20200906063609_create_relationships.rb
@@ -5,6 +5,13 @@ class CreateRelationships < ActiveRecord::Migration[5.2]
       t.references :followed, null: false, foreign_key: { to_table: :users }
 
       t.timestamps
+      t.index [:follower_id, :followed_id], unique: true
+      # follower_idとfollowed_idの組み合わせにユニーク制約をする。
+
+      # 見本ではそれぞれのカラムに対してindexが貼られていたが、見本と違い"references型"で
+      # カラムを作成したためか、schemaでは既にindexが適用されていたため記述省略。
+      # →なぜ見本ではintger型なのか？
+      # いいねモデルと同じく、references型で良いのでは？と思っています。もし理由があるなら教えていただけるとありがたいです。
     end
   end
 end
