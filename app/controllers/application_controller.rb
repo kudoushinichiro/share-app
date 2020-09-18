003d8d1944b4e6de57fc70bb0bff49e43a9ca3ab
kudpig <shin.kudo.aa@gmail.com>
2020-09-18 22:56:39 +0900

    rubocopの指摘修正
    rubocop --auto-correctコマンド実行

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   before_action :set_search_posts_form
   add_flash_types :success, :info, :warning, :danger
 
-
   def not_authenticated
     redirect_to login_path, warning: 'ログインしてください'
     # not_authenticatedはsorceryにデフォルトで設定されているメソッド。require_loginに引っかかった時に実行される。
