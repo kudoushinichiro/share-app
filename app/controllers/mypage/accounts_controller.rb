668442ea6ae2ae3e76abe490832afea2955176df
kudpig <shin.kudo.aa@gmail.com>
2020-09-22 15:12:43 +0900

    flashをシンボルに変更

--- app/controllers/mypage/accounts_controller.rb
+++ app/controllers/mypage/accounts_controller.rb
@@ -13,7 +13,7 @@ class Mypage::AccountsController < Mypage::BaseController
       # @userをaccount_paramsの内容で上書きする
       redirect_to edit_mypage_account_path, success: 'プロフィールを更新しました'
     else
-      flash.now['danger'] = 'プロフィールの更新に失敗しました'
+      flash.now[:danger] = 'プロフィールの更新に失敗しました'
       render :edit
     end
   end
